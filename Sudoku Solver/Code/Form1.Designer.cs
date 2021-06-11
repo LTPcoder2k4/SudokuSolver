d0d4d312764e086dbf1774532282bd86fb0ff80d
Lê Tấn Phát <84580263+LTPcoder2k4@users.noreply.github.com>
2021-06-11 22:49:32 +0700

    Add files via upload

---
+++ Sudoku Solver/Code/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.importBtn = new Sudoku.RJButton();
+            this.solveBtn = new Sudoku.RJButton();
+            this.newBtn = new Sudoku.RJButton();
+            this.sudokuBox = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.groupBox2.Controls.Add(this.importBtn);
+            this.groupBox2.Controls.Add(this.solveBtn);
+            this.groupBox2.Controls.Add(this.newBtn);
+            this.groupBox2.Font = new System.Drawing.Font("GamePixies", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.ForeColor = System.Drawing.Color.Cyan;
+            this.groupBox2.Location = new System.Drawing.Point(610, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(178, 426);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Handle";
+            // 
+            // importBtn
+            // 
+            this.importBtn.BackColor = System.Drawing.Color.SlateBlue;
+            this.importBtn.BackgroundColor = System.Drawing.Color.SlateBlue;
+            this.importBtn.BorderColor = System.Drawing.Color.Indigo;
+            this.importBtn.BorderRadius = 40;
+            this.importBtn.BorderSize = 3;
+            this.importBtn.FlatAppearance.BorderSize = 0;
+            this.importBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.importBtn.ForeColor = System.Drawing.Color.White;
+            this.importBtn.Location = new System.Drawing.Point(6, 293);
+            this.importBtn.Name = "importBtn";
+            this.importBtn.Size = new System.Drawing.Size(166, 54);
+            this.importBtn.TabIndex = 2;
+            this.importBtn.Text = "Import";
+            this.importBtn.TextColor = System.Drawing.Color.White;
+            this.importBtn.UseVisualStyleBackColor = false;
+            this.importBtn.Click += new System.EventHandler(this.importBtn_Click);
+            // 
+            // solveBtn
+            // 
+            this.solveBtn.BackColor = System.Drawing.Color.SlateBlue;
+            this.solveBtn.BackgroundColor = System.Drawing.Color.SlateBlue;
+            this.solveBtn.BorderColor = System.Drawing.Color.Indigo;
+            this.solveBtn.BorderRadius = 40;
+            this.solveBtn.BorderSize = 3;
+            this.solveBtn.FlatAppearance.BorderSize = 0;
+            this.solveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.solveBtn.ForeColor = System.Drawing.Color.White;
+            this.solveBtn.Location = new System.Drawing.Point(6, 182);
+            this.solveBtn.Name = "solveBtn";
+            this.solveBtn.Size = new System.Drawing.Size(166, 54);
+            this.solveBtn.TabIndex = 1;
+            this.solveBtn.Text = "Solve";
+            this.solveBtn.TextColor = System.Drawing.Color.White;
+            this.solveBtn.UseVisualStyleBackColor = false;
+            this.solveBtn.Click += new System.EventHandler(this.solveBtn_Click);
+            // 
+            // newBtn
+            // 
+            this.newBtn.BackColor = System.Drawing.Color.SlateBlue;
+            this.newBtn.BackgroundColor = System.Drawing.Color.SlateBlue;
+            this.newBtn.BorderColor = System.Drawing.Color.Indigo;
+            this.newBtn.BorderRadius = 40;
+            this.newBtn.BorderSize = 3;
+            this.newBtn.FlatAppearance.BorderSize = 0;
+            this.newBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newBtn.ForeColor = System.Drawing.Color.White;
+            this.newBtn.Location = new System.Drawing.Point(6, 72);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(166, 54);
+            this.newBtn.TabIndex = 0;
+            this.newBtn.Text = "New";
+            this.newBtn.TextColor = System.Drawing.Color.White;
+            this.newBtn.UseVisualStyleBackColor = false;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // sudokuBox
+            // 
+            this.sudokuBox.BackColor = System.Drawing.Color.MidnightBlue;
+            this.sudokuBox.Font = new System.Drawing.Font("GamePixies", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sudokuBox.ForeColor = System.Drawing.Color.Cyan;
+            this.sudokuBox.Location = new System.Drawing.Point(12, 12);
+            this.sudokuBox.Name = "sudokuBox";
+            this.sudokuBox.Size = new System.Drawing.Size(592, 426);
+            this.sudokuBox.TabIndex = 2;
+            this.sudokuBox.TabStop = false;
+            this.sudokuBox.Text = "Sudoku";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.sudokuBox);
+            this.ForeColor = System.Drawing.Color.Cyan;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku Solver";
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox sudokuBox;
+        private RJButton solveBtn;
+        private RJButton newBtn;
+        private RJButton importBtn;
+    }
+}
+
---
+++ Sudoku Solver/Code/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        const int n = 9;
+        const int btnSize = 45;
+        RJButton[,] btnMap = new RJButton[n, n];
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateMap();
+        }
+        private void GenerateMap()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    btnMap[i, j] = new RJButton();
+                    btnMap[i, j].Size = new Size(btnSize, btnSize);
+                    btnMap[i, j].TextColor = Color.Cyan;
+                    btnMap[i, j].Text = "";
+                    btnMap[i, j].Location = new Point(j * btnSize + 100, i * btnSize + 15);
+                    if (((2 < j && j < 6) && (i < 3 || i > 5)) || ((2 < i && i < 6) && (j < 3 || j > 5)))
+                    {
+                        btnMap[i, j].BackColor = Color.RoyalBlue;
+                    }
+                    btnMap[i, j].Click += new EventHandler(btnClick);
+                    sudokuBox.Controls.Add(btnMap[i, j]);
+                }
+            }
+        }
+
+        private void Clear()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    btnMap[i, j].Text = "";
+                    btnMap[i, j].Click += new EventHandler(btnClick);
+                    btnMap[i, j].TextColor = Color.Cyan;
+                }
+            }
+        }
+
+        private bool checkInput()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (btnMap[i, j].Text != "")
+                    {
+                        for (int k = i + 1; k < 9; ++k)  // kiem tra theo hang
+                        {
+                            if (btnMap[i, j].Text != "")
+                            {
+                                if (btnMap[i, j].Text == btnMap[k, j].Text)
+                                    return false;
+                            }
+                        }
+                        for (int k = 0; k < i; ++k)  // kiem tra theo hang
+                        {
+                            if (btnMap[i, j].Text != "")
+                            {
+                                if (btnMap[i, j].Text == btnMap[k, j].Text)
+                                    return false;
+                            }
+                        }
+                        for (int k = j + 1; k < 9; ++k)  // kiem tra theo hang
+                        {
+                            if (btnMap[i, j].Text != "")
+                            {
+                                if (btnMap[i, j].Text == btnMap[i, k].Text)
+                                    return false;
+                            }
+                        }
+                        for (int k = 0; k < j; ++k)  // kiem tra theo hang
+                        {
+                            if (btnMap[i, j].Text != "")
+                            {
+                                if (btnMap[i, j].Text == btnMap[i, k].Text)
+                                    return false;
+                            }
+                        }
+                        int boxRowOffset = (i / 3) * 3;
+                        int boxColOffset = (j / 3) * 3;
+
+                        for (int k = 0; k < 3; ++k) //kiem tra trong 9 ô nhỏ
+                            for (int m = 0; m < 3; ++m)
+                                if ((boxRowOffset + k) != i && boxColOffset + m != j)
+                                {
+                                    if (btnMap[boxRowOffset + k, boxColOffset + m].Text != "")
+                                    {
+                                        if (btnMap[i, j].Text == btnMap[boxRowOffset + k, boxColOffset + m].Text)
+                                            return false;
+                                    }
+                                }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static bool solve(RJButton[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j].Text == "")
+                    {
+                        for (char c = '1'; c <= '9'; c++)
+                        {
+                            if (isValid(board, i, j, c))
+                            {
+                                board[i, j].Text = c.ToString();
+
+                                if (solve(board))
+                                    return true;
+                                else
+                                {
+                                    board[i, j].Text = "";
+                                }
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static bool isValid(RJButton[,] board, int row, int col, char c)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                //check row  
+                if (board[i, col].Text != "" && board[i, col].Text == c.ToString())
+                    return false;
+                //check column  
+                if (board[row, i].Text != "" && board[row, i].Text == c.ToString())
+                    return false;
+                //check 3*3 block  
+                if (board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3].Text != "" && board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3].Text == c.ToString())
+                    return false;
+            }
+            return true;
+        }
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            int num;
+            RJButton btn = (RJButton)sender;
+            int y = (btn.Location.X - 100) / btnSize;
+            int x = (btn.Location.Y - 15) / btnSize;
+            if (btnMap[x, y].Text == "") num = 0;
+            else num = int.Parse(btnMap[x, y].Text);
+
+            num = (num + 1) % 10;
+            if (num == 0) btnMap[x, y].Text = "";
+            else btnMap[x, y].Text = num.ToString();
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void solveBtn_Click(object sender, EventArgs e)
+        {
+            if (checkInput())
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (btnMap[i, j].Text == "")
+                            btnMap[i, j].TextColor = Color.Red;
+                    }
+                }
+                if (!solve(btnMap)) MessageBox.Show("Can't solve");
+            }
+            else
+            {
+                MessageBox.Show("Please check the input!");
+            }
+        }
+
+        private void importBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "txt file|*.txt";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                Clear();
+                string filename = op.FileName;
+                string[] filelines = File.ReadAllLines(filename);
+                if (filelines.Length == 9)
+                {
+                    for (int j = 0; j < filelines.Length; j++)
+                    {
+                        string[] splitLines = filelines[j].Split(' ');
+                        if (splitLines.Length == 9)
+                        {
+                            for (int i = 0; i < splitLines.Length; i++)
+                            {
+                                if (int.Parse(splitLines[i]) >= 0 && int.Parse(splitLines[i]) <= 9)
+                                {
+                                    if (int.Parse(splitLines[i]) == 0)
+                                    {
+                                        btnMap[j, i].Text = "";
+                                    }
+                                    else
+                                    {
+                                        btnMap[j, i].Text = splitLines[i];
+                                    }
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Error Reading File");
+                                    Clear();
+                                    return;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error Reading File");
+                            Clear();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error Reading File");
+                    Clear();
+                    return;
+                }
+            }
+        }
+    }
+}
